ba6849cc8f5fc43ff3718f0906ba3b4ec433e350
David Wang <David.Wang@quantatw.com>
2020-07-09 19:01:59 +0800

    upfate 20200709

--- app/assets/config/manifest.js
+++ app/assets/config/manifest.js
@@ -1,4 +1,5 @@
 //= link_tree ../images
+//= link_tree ../fonts
 //= link_directory ../stylesheets .css
 //= link_directory ../fonts/material-icon/css .css
 //= link_directory ../fonts/poppins .svg
@@ -6,4 +7,5 @@
 //= link_directory ../fonts/poppins .ttf
 //= link_directory ../fonts/poppins .woff
 //= link_directory ../fonts/poppins .woff2
+//= link_directory ../fonts/material-icon/css .css
 //= link application.js
